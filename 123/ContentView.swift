1fcc9f9cb59a69977809e7596dc19b7c233e9a3d
YuanShenKing <z20230404@gmail.com>
2024-03-09 23:24:42 +0800

    Initial Commit

---
+++ 123/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  123
+//
+//  Created by 原神 on 2024/3/9.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 123/_23App.swift
@@ -0,0 +1,17 @@
+//
+//  _23App.swift
+//  123
+//
+//  Created by 原神 on 2024/3/9.
+//
+
+import SwiftUI
+
+@main
+struct _23App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
